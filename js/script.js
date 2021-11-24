27bc2d825be3a39d191b4f71dcbd5a70e96cf050
Seweryn Kuliga <skuligas@gmail.com>
2021-11-24 12:27:48 +0100

    Update
    - navbar height set

--- js/script.js
+++ js/script.js
@@ -6,26 +6,49 @@ window.onload = () => {
   let logo = document.querySelector(".logo");
   let spans = document.querySelectorAll(".spans");
   let toExpandButtons = document.querySelector(".expand_collapse_menu");
+  var accordion = document.getElementsByClassName("accordion");
+  let accPanel = document.querySelectorAll(".panel");
+  var i;
 
-  showLessInfo.style.display = "none";
-  toExpandButtons.style.display = "flex";
+  // This function expands the panel
   function showMore() {
-    leftMenu.style.width = "200px";
+    leftMenu.style.width = "300px";
     showLessInfo.style.display = "block";
     showMoreInfo.style.display = "none";
-    logo.style.just;
     buttonFields.forEach((button) => (button.style.display = "flex"));
     buttonFields.forEach((button) => (button.style.height = "35px"));
     spans.forEach((span) => (span.style.display = "inline"));
   }
+
+  // This function collapses the navbar
   function showLess() {
     leftMenu.style.width = "50px";
     showLessInfo.style.display = "none";
     showMoreInfo.style.display = "block";
+    accPanel.forEach((accpanel) => (accpanel.style.display = "none"));
     buttonFields.forEach((button) => (button.style.display = "block"));
     spans.forEach((span) => (span.style.display = "none"));
   }
 
   showMoreInfo.addEventListener("click", showMore);
   showLessInfo.addEventListener("click", showLess);
+
+
+  for (i = 0; i < accordion.length; i++) {
+    accordion[i].addEventListener("click", function () {
+      // Toggle between adding and removing the "active" class, to highlight the button that controls the panel
+      this.classList.toggle("active");
+
+      // Toggle between hiding and showing the active panel
+      if (leftMenu.style.width === "300px") {
+        let panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+          panel.style.display = "none";
+        } else {
+          panel.style.display = "block";
+        }
+      }
+    });
+    }
+  
 };
